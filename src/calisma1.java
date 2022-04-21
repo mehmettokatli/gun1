5ce1e7c3289867527c0061e81d7b3c23539fe2e8
mehmettokatli <101505366+mehmettokatli@users.noreply.github.com>
2022-04-21 13:42:38 +0300

    öğle arası sonrası çalışma kontrol

--- src/calisma1.java
+++ src/calisma1.java
@@ -1,5 +1,6 @@
 public class calisma1 {
     public static void main(String[] args) {
         System.out.println("öğlen arası");
+        System.out.println("öğlen arası sonrası kontrol");
     }
 }
